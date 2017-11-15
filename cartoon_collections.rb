690e554d12bc05c4d8bc52138fd0aea2e2985ee8
John Wojciechowski <jwojo13@gmail.com>
2017-11-15 16:00:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,7 @@ def summon_captain_planet(planeteers)
   heroes = []
   planeteers.collect do |hero|
     heroes << hero.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
